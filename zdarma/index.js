306ce0547723eaeae80dcd6d88df54ce132848d2
Raferat <mailnaneduveryhodnestranky@seznam.cz>
2021-04-12 12:52:59 +0200

    Updated

--- zdarma/index.js
+++ zdarma/index.js
@@ -1,4 +1,16 @@
+let i = document . getElementById ("cursor");
+
 function moveCursor(event)
 {
+  var x = event . clientX;
+  var y = event . clientY + window.pageYOffset;
   
+  i . style . display = "block";
+  i . style . top = y + "px";
+  i . style . left = x + "px";
+}
+
+function hideCursor ()
+{
+  i . style . display = "none";
 }
