0f032be2987314560092bca5093dce0824b38fb8
Raferat <mailnaneduveryhodnestranky@seznam.cz>
2021-04-14 10:54:30 +0200

    Updated

--- zdarma/index.js
+++ zdarma/index.js
@@ -3,7 +3,7 @@ let i = document . getElementById ("cursor");
 function moveCursor(event)
 {
   var x = event . clientX;
-  var y = event . clientY + window.pageYOffset;
+  var y = event . clientY;
   
   i . style . display = "block";
   i . style . top = y + "px";
@@ -14,3 +14,8 @@ function hideCursor ()
 {
   i . style . display = "none";
 }
+
+$(window).on("load",function()
+{
+  $(".loader-wrapper") . fadeOut(10000);
+});
